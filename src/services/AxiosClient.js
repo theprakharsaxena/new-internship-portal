12e87c71f9f6af15853f26ac65b41ba09f97d9b3
theprakharsaxena <prakharsaxena5125@gmail.com>
2023-08-27 20:56:56 +0530

    drtfgbyhj

--- src/services/AxiosClient.js
+++ src/services/AxiosClient.js
@@ -4,10 +4,4 @@ const AxiosClient = axios.create();
 
 AxiosClient.defaults.baseURL = "https://internship-43hq.onrender.com";
 
-AxiosClient.defaults.headers = {
-  "Content-Type": "application/json",
-};
-
-AxiosClient.defaults.withCredentials = true;
-
 export default AxiosClient;
